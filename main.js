6698a01135200ea36136ff6407f0172e2721b13a
Karen Montes <karen.montes04@gmail.com>
2019-11-30 10:55:10 -0700

    learned about character table generator, using that instead

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
-const numbers = '0123456789';
-const upperLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const lowerLetters = 'abcdefghijklmnopqrstuvwxyz';
-const symbols = '!#$%&'()*+,-./:;<=>?@[\]^_`{|}~';
+function lowerCaseLetter() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
 
+function upperCaseLetter() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getNumbers() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getSymbols() {
+    return String.fromCharCode(Math.floor(Math.random() * 15) + 33);
+}
+console.log(getSymbols());
