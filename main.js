4dfd06e9c84a379ac9bc1602212b03fcf4941332
Karen Montes <karen.montes04@gmail.com>
2019-12-02 20:13:42 -0700

    trying new loop for intial statements

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+const confNumbers = confirm(Would you like numbers in your password?);
+
+
+
+
+
+
+
+
+
+
+
+const randomPassword = {
+    lower: lowerCaseLetter,
+    upper: upperCaseLetter,
+    numbers: getNumbers,
+    symbols: getSymbols
+
+};
+
 function lowerCaseLetter() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
@@ -13,4 +33,5 @@ function getNumbers() {
 function getSymbols() {
     return String.fromCharCode(Math.floor(Math.random() * 15) + 33);
 }
-console.log(getSymbols());
+
+
